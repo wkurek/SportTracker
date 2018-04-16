c36a4ffdec6dd44baaf14712ba4ecfbbb0f868cd
wkurek <w.kurek2@gmail.com>
2018-04-16 21:32:21 +0200

    Fetching data required by PersonalFragment from database

--- app/src/main/java/com/wkurek/sporttracker/DbHelper.java
+++ app/src/main/java/com/wkurek/sporttracker/DbHelper.java
@@ -40,6 +40,13 @@ public class DbHelper extends SQLiteOpenHelper {
         static final String COLUMN_NAME_DISTANCE = "distance";
         static final String COLUMN_NAME_TRACK = "track";
         static final String COLUMN_NAME_LOCATIONS = "locations";
+
+        static final String COLUMN_NAME_MAX_DISTANCE = "max_distance";
+        static final String COLUMN_NAME_MAX_SECONDS_NUMBER = "max_seconds_number";
+
+        static final String COLUMN_NAME_TRAININGS_NUMBER = "training_number";
+        static final String COLUMN_NAME_SUM_DISTANCE = "distance_sum";
+        static final String COLUMN_NAME_SUM_SECONDS_NUMBER = "seconds_number_sum";
     }
 
     DbHelper(Context context) {
@@ -57,10 +64,10 @@ public class DbHelper extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase); // and create a new one
     }
 
-   private Cursor select(String table, String[] columns,  String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
-        SQLiteDatabase database = this.getReadableDatabase();
+   private Cursor select(String table, String[] columns,  String orderBy) {
+       SQLiteDatabase database = this.getReadableDatabase();
        database.enableWriteAheadLogging();
-        return database.query(table, columns, selection, selectionArgs, groupBy, having, orderBy);
+       return database.query(table, columns, null, null, null, null, orderBy);
    }
 
    Cursor selectAllTrainings() {
@@ -69,8 +76,7 @@ public class DbHelper extends SQLiteOpenHelper {
                 TrainingContract.COLUMN_NAME_SECONDS_NUMBER, TrainingContract.COLUMN_NAME_TRACK};
         String orderBy = String.format(Locale.GERMANY, "%s DESC", TrainingContract.COLUMN_NAME_START_TIME);
 
-        return select(TrainingContract.TABLE_NAME, columns, null, null, null,
-                null, orderBy);
+        return select(TrainingContract.TABLE_NAME, columns, orderBy);
    }
 
    void insertTraining(ContentValues values) {
@@ -81,5 +87,30 @@ public class DbHelper extends SQLiteOpenHelper {
         if(result > 0) {
             Log.i(TAG, "Training inserted to database correctly.");
         } else Log.i(TAG, "Error while inserting training to database.");
+   }
+
+   Cursor selectPeriodSummary(long startTime) {
+       String query = String.format(Locale.GERMANY, "SELECT COUNT(%s) AS %s, SUM(%s) AS %s, SUM(%s) AS %s FROM %s WHERE %s > %d;",
+               BaseColumns._ID, TrainingContract.COLUMN_NAME_TRAININGS_NUMBER, TrainingContract.COLUMN_NAME_DISTANCE,
+               TrainingContract.COLUMN_NAME_SUM_DISTANCE, TrainingContract.COLUMN_NAME_SECONDS_NUMBER,
+               TrainingContract.COLUMN_NAME_SUM_SECONDS_NUMBER, TrainingContract.TABLE_NAME,
+               TrainingContract.COLUMN_NAME_START_TIME, startTime);
+
+       SQLiteDatabase database = this.getReadableDatabase();
+       database.enableWriteAheadLogging();
+
+       return database.rawQuery(query, null);
+   }
+
+   Cursor selectPersonalBests() {
+       String query = String.format(Locale.GERMANY, "SELECT MAX(%s) AS %s, MAX(%s) AS %s FROM %s;",
+               TrainingContract.COLUMN_NAME_DISTANCE, TrainingContract.COLUMN_NAME_MAX_DISTANCE,
+               TrainingContract.COLUMN_NAME_SECONDS_NUMBER, TrainingContract.COLUMN_NAME_MAX_SECONDS_NUMBER,
+               TrainingContract.TABLE_NAME);
+
+       SQLiteDatabase database = this.getReadableDatabase();
+       database.enableWriteAheadLogging();
+
+       return database.rawQuery(query, null);
    }
 }
--- app/src/main/java/com/wkurek/sporttracker/PersonalFragment.java
+++ app/src/main/java/com/wkurek/sporttracker/PersonalFragment.java
@@ -1,29 +1,98 @@
 package com.wkurek.sporttracker;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PersonalFragment extends Fragment {
+    private static final String TAG = PersonalFragment.class.getSimpleName();
 
-
-    public PersonalFragment() {
-        // Required empty public constructor
-    }
-
+    public PersonalFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_personal, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        DbHelper helper = new DbHelper(getActivity());
+
+        Cursor totalCursor = helper.selectPeriodSummary(0);
+        if(totalCursor.getCount() == 1) fillTotalSummary(view, totalCursor);
+
+        Cursor monthCursor = helper.selectPeriodSummary(getCurrentMonthTimestamp());
+        if(monthCursor.getCount() == 1) fillMonthSummary(view, monthCursor);
+
+        Cursor personalBestsCursor = helper.selectPersonalBests();
+        if(personalBestsCursor.getCount() == 1) fillHallOfFame(view, personalBestsCursor);
+
+
+    }
+
+    private void fillTotalSummary(View layout, Cursor cursor) {
+        cursor.moveToFirst();
+
+        int trainingsNumberIndex = cursor.getColumnIndex(DbHelper.TrainingContract.COLUMN_NAME_TRAININGS_NUMBER);
+        int secondsNumberSumIndex = cursor.getColumnIndex(DbHelper.TrainingContract.COLUMN_NAME_SUM_SECONDS_NUMBER);
+        int distanceSumIndex = cursor.getColumnIndex(DbHelper.TrainingContract.COLUMN_NAME_SUM_DISTANCE);
+
+        //TODO: fill TextViews with data. Use NotationGenerator, take values from cursor
+
+        cursor.close();
+    }
+
+    private void fillMonthSummary(View layout, Cursor cursor) {
+        cursor.moveToFirst();
+
+        int trainingsNumberIndex = cursor.getColumnIndex(DbHelper.TrainingContract.COLUMN_NAME_TRAININGS_NUMBER);
+        int secondsNumberSumIndex = cursor.getColumnIndex(DbHelper.TrainingContract.COLUMN_NAME_SUM_SECONDS_NUMBER);
+        int distanceSumIndex = cursor.getColumnIndex(DbHelper.TrainingContract.COLUMN_NAME_SUM_DISTANCE);
+
+        //TODO: fill TextViews with data. Use NotationGenerator, take values from cursor
+
+        cursor.close();
+    }
+
+    private void fillHallOfFame(View layout, Cursor cursor) {
+        cursor.moveToFirst();
+
+        int secondsNumberMaxIndex = cursor.getColumnIndex(DbHelper.TrainingContract.COLUMN_NAME_MAX_SECONDS_NUMBER);
+        int distanceMaxIndex = cursor.getColumnIndex(DbHelper.TrainingContract.COLUMN_NAME_MAX_DISTANCE);
+
+        TextView distanceView = layout.findViewById(R.id.personal_best_distance);
+        TextView timeView = layout.findViewById(R.id.personal_best_time);
+
+        distanceView.setText(NotationGenerator.generateDistanceNotation(cursor.getDouble(distanceMaxIndex)));
+        timeView.setText(NotationGenerator.generateTimeNotation(cursor.getLong(secondsNumberMaxIndex)));
+
+        cursor.close();
+    }
+
+    private long getCurrentMonthTimestamp() {
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.getMinimum(Calendar.DAY_OF_MONTH));
+            calendar.set(Calendar.HOUR_OF_DAY, calendar.getMinimum(Calendar.HOUR_OF_DAY));
+            calendar.set(Calendar.MINUTE, calendar.getMinimum(Calendar.MINUTE));
+
+        return calendar.getTimeInMillis();
+    }
 }
